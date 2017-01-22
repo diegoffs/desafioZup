0d57f33c8934a4e1b4aa78b5c95206b7204924dd
diegofs <diegoffs1995@gmail.com>
2017-01-22 13:27:43 -0200

    Fix JOptionPane

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # desafioZup
-Projeto de desafio da Zup desenvolvido em JAVA
+Projeto de desafio da Zup desenvolvido em JAVA.
+
+
+Para rodar o programa entre na pasta dist e dê dois cliques no arquivo DesafioZup.jar.
+
+Caso queira executar pela linha de comando entre na pasta dist e digite: java -jar DesafioZup.jar
+
+Após iniciar o programa digite as extensões X e Y máximas da plataforma separadas por espaço, respectivamente:
+
+Exemplo: 4 8
+
+Clique em OK.
+
+Ná próxima tela, digite a posição atual (X e Y) e o sentido inicial separados por espaço, respectivamente:
+
+Exemplo: 1 2 N
+
+Clique em OK.
+
+Por fim, digite a sequência de instruções (L, R ou M) para o Rover na Plataforma em Marte:
+
+Exemplo: RRMMLMMR
+
+Clique em OK.
+
+Caso queira mandar outro Rover pra Marte clique Enviar.
+
+Clique em Cancelar para finalizar o programa.
--- src/desafiozup/Main.java
+++ src/desafiozup/Main.java
@@ -6,6 +6,7 @@
 package desafiozup;
 
 import java.util.Scanner;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 /**
@@ -19,9 +20,13 @@ public class Main {
      */
     public static void main(String[] args) {
         
-
+        JFrame frame = new JFrame("Controle Rovers");
+        frame.setUndecorated( true );
+        frame.setVisible( true );
+        frame.setLocationRelativeTo( null );
+        
         Scanner sc = new Scanner(System.in);
-        String firstLine = JOptionPane.showInputDialog(null, "Digite as extensões X e Y máximas da plataforma separadas por espaço (Exemplo:2 3 )", "Extensões" , -1);
+        String firstLine = JOptionPane.showInputDialog(frame, "Digite as extensões X e Y máximas da plataforma separadas por espaço (Exemplo:2 3 )", "Extensões" , -1);
         
         if(firstLine == null) {
             System.exit(0);
@@ -32,7 +37,7 @@ public class Main {
         
         InputValidator validator = new InputValidator();
         while(validator.firstLineInputValidation(plateauCoordinates)) {
-            firstLine = JOptionPane.showInputDialog(null, "Digite as extensões X e Y máximas da plataforma separadas por espaço (Exemplo:2 3 )", "Extensões" , -1);
+            firstLine = JOptionPane.showInputDialog(frame, "Digite as extensões X e Y máximas da plataforma separadas por espaço (Exemplo:2 3 )", "Extensões" , -1);
                 if(firstLine == null) {
                 System.exit(0);
             }
@@ -49,7 +54,7 @@ public class Main {
         
         while (retorno == 0) {
 
-            String nextRoverPosition = JOptionPane.showInputDialog(null, "Digite a posição atual e o sentido inicial separados por espaço (Exemplo:1 4 E )", "Posição do Rover" , -1);
+            String nextRoverPosition = JOptionPane.showInputDialog(frame, "Digite a posição atual e o sentido inicial separados por espaço (Exemplo:1 4 E )", "Posição do Rover" , -1);
             if(nextRoverPosition == null){
                 System.exit(0);
             }
@@ -63,7 +68,7 @@ public class Main {
                             platform.getxMaxPlatform(),platform.getyMaxPlatform())
                     || validator.cardinalCompassPointsValidation(splittedNextRoverPosition[2])) {
                 
-                nextRoverPosition = JOptionPane.showInputDialog(null, "Digite a posição atual e o sentido inicial separados por espaço (Exemplo:1 4 E )", "Posição do Rover" , -1);
+                nextRoverPosition = JOptionPane.showInputDialog(frame, "Digite a posição atual e o sentido inicial separados por espaço (Exemplo:1 4 E )", "Posição do Rover" , -1);
                 if(nextRoverPosition == null){
                     System.exit(0);
                 }
@@ -79,7 +84,7 @@ public class Main {
 
             Rover rover = new Rover(posicao, platform);
 
-            String nextRoverCommands = JOptionPane.showInputDialog(null, "Digite a sequência de instruções para o Rover na Plataforma em Marte", "Sequênca de comandos" , -1);
+            String nextRoverCommands = JOptionPane.showInputDialog(frame, "Digite a sequência de instruções para o Rover na Plataforma em Marte", "Sequênca de comandos" , -1);
             if(nextRoverCommands == null) {
                 System.exit(0);
             }
@@ -89,7 +94,7 @@ public class Main {
             char[] roverCommands = nextRoverCommands.toCharArray();
             
             while(validator.roverCommandsSequenceValidation(roverCommands)) {
-                nextRoverCommands = JOptionPane.showInputDialog(null, "Digite a sequência de instruções para o Rover na Plataforma em Marte", "Sequênca de comandos" , -1);
+                nextRoverCommands = JOptionPane.showInputDialog(frame, "Digite a sequência de instruções para o Rover na Plataforma em Marte", "Sequênca de comandos" , -1);
                 if(nextRoverCommands == null) {
                     System.exit(0);
                 }
@@ -101,10 +106,12 @@ public class Main {
 
             Position resultado = rover.walkThroughPlatform(roverCommands);
 
-            JOptionPane.showMessageDialog(null, resultado.getX() + " " + resultado.getY() + " " + resultado.getLocation(), "Posição Final", -1 );
-
-            retorno = JOptionPane.showOptionDialog(null, "Deseja enviar outro Rover", "Enviar Novo Rover?", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+            JOptionPane.showMessageDialog(frame, resultado.getX() + " " + resultado.getY() + " " + resultado.getLocation(), "Posição Final", -1 );
+            
+            retorno = JOptionPane.showOptionDialog(frame, "Deseja enviar outro Rover", "Enviar Novo Rover?", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
         }
+        
+        frame.dispose();
         
     }
     
